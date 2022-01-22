79cc4d8970b974ddaa20276b910d60b0c8713c3c
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2022-01-22 14:42:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,218 @@
+# netlify-functions-session-cookie 🍪
+Cryptographically-signed session cookies for [Netlify functions](https://docs.netlify.com/functions/overview/).
+
+---
+
+## Summary 
+- [Install](#Install)
+- [Concept and Usage](#concept-and-usage)
+- [API](#api)
+- [Environment variables and options](#environment-variables-and-options)
+- [Generating a secret key](#generating-a-secret-key)
+- [Notes and misc](#notes-and-misc)
+- [Contributing](#contributing)
+
+---
+
+## Install
+
+```bash
+npm install netlify-functions-session-cookie
+```
+
+⚠️ This library requires a **secret key** to sign and verify cookies. 
+See [_"Generating a secret key"_](#generating-a-secret-key).
+
+[☝️ Back to summary](#summary)
+
+---
+
+## Concept and usage
+This library automatically manages a cryptographically-signed cookie that can be used to store data for a given client. 
+Signed cookies are harder to tamper with and can therefore be used to store non-sensitive data on the client side. 
+
+**It takes inspiration from [Flask's default session system](https://flask.palletsprojects.com/en/2.0.x/quickstart/#sessions) and behaves in a similar way:** 
+- **At handler level:** gives access to a standard object which can be used to read and write data to and from the session cookie for the current client.
+- **Behind the scenes:** the session cookie is automatically verified and parsed on the way in, signed and serialized on the way out. 
+
+Simply wrap a Netlify Function handler with `withSession()` to get started. 
+
+### Example: assigning an identifier to a client.
+```javascript
+const { withSession, getSession } = require('../index.js');
+const crypto = require('crypto');
+
+exports.handler = withSession(async function(event, context) {
+
+  const session = getSession(context);
+
+  // `session.identifier` will already exist the next time this client sends a request.
+  if (!session.identifier) {
+    session.identifier = crypto.randomBytes(16).toString('hex');
+  }
+
+  return {
+    statusCode: 200,
+    body: `Your identifier is ${session.identifier}`
+  }
+  
+});
+```
+
+The `Set-Cookie` header is automatically added to the response object to include a serialized and signed version of the session object:
+
+```javascript
+// `response` object
+{
+  statusCode: 200,
+  body: 'Your identifier is 167147eb57500c660bce192a0debeb58',
+  multiValueHeaders: {
+    'Set-Cookie': [
+      'session=E58l2HhxWQycgAedPvt2g-hfr96j06tLJ0f4t0KRuOseyJpZGVudGlmaWVyIjoiMTY3MTQ3ZWI1NzUwMGM2NjBiY2UxOTJhMGRlYmViNTgifQ%3D%3D; Max-Age=604800; Path=/; HttpOnly; Secure; SameSite=Lax'
+    ]
+  }
+}
+```
+
+> **Note:** Existing `Set-Cookie` entries in `response.headers` or `response.multiValueHeaders` are preserved and merged into `response.multiValueHeaders`. 
+
+The cookie's attributes can be configured individually using [environment variables](#environment-variables-and-options).
+
+
+[☝️ Back to summary](#summary)
+
+---
+
+## API
+
+### withSession(handler: AsyncFunction)
+Takes a [synchronous Netlify Function handler](https://docs.netlify.com/functions/build-with-javascript/#synchronous-function-format) as an argument and returns it wrapped with [`sessionWrapper()`](https://github.com/matteocargnelutti/netlify-functions-session-cookie/blob/main/index.js#:~:text=function%20sessionWrapper), which handles the session cookie in and out. 
+
+See [_"Concept and Usage"_](#concept-and-usage) for more information.
+
+```javascript
+const { withSession } = require('netlify-functions-session-cookie');
+
+exports.handler = withSession(async function(event, context) {
+  // ...
+});
+
+// Alternatively: 
+async function(event, context) {
+  // ...
+}
+exports.handler = withSession(handler); 
+
+```
+
+### getSession(context: Object)
+`getSession()` takes a `context` object from a Netlify Function handler as an argument a returns a reference to `context.clientContext.sessionCookieData`, which is where parsed session data live.
+
+If `context.clientContext.sessionCookieData` doesn't exist, it is going to be created on the fly.
+
+```javascript
+const { withSession } = require('netlify-functions-session-cookie');
+
+exports.handler = withSession(async function(event, context) {
+  const session = getSession(context); 
+  // `session` can now be used to read and write data from the session cookie.
+  // ...
+});
+```
+
+### clearSession(context: Object)
+As the session object is passed to the Netlify Functions handler by reference, it is not possible to empty by simply replacing it by a new object:
+
+```javascript
+exports.handler = withSession(async function(event, context) {
+  let session = getSession(context);
+  session = {}; // This will NOT clear session data.
+  // ...
+});
+```
+
+You may instead use the `clearSession()` function to do so. This function takes the Netlify Functions handler `context` object as an argument.
+
+```javascript
+const { withSession, getSession, clearSession } = require('netlify-functions-session-cookie');
+
+async function handler(event, context) {
+
+  const session = getSession(session);
+  clearSession(session); // Will clear the session object in place.
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Session cookie cleared." }),
+  };
+  
+}
+exports.handler = withSession(handler);
+```
+
+### generateSecretKey()
+Generates and returns a 32-byte-long random key, encoded in base64.
+See [_"Generating a secret key"_](#generating-a-secret-key).
+
+[☝️ Back to summary](#summary)
+
+---
+
+## Environment variables and options
+
+The session cookie can be configured through environment variables.
+
+### Required
+| Name | Description |
+| --- | --- |
+| `SESSION_COOKIE_SECRET` | Used to sign and validate the session cookie. Must be at least 32 bytes long. See [_"Generating a secret key"_](#generating-a-secret-key) for more information. |
+
+### Optional
+| Name | Description |
+| --- | --- |
+| `SESSION_COOKIE_NAME` | Name used by the session cookie. Must only contain ASCII-compatible characters and no whitespace. Defaults to `"session"`. |
+| `SESSION_COOKIE_HTTPONLY` | The session cookie bears the `HttpOnly` attribute by default. Set this environment variable to `"0"` to remove it. | 
+| `SESSION_COOKIE_SECURE` | The session cookie bears the `Secure` attribute by default. Set this environment variable to `"0"` to remove it. | 
+| `SESSION_COOKIE_SAMESITE` | Can be `"Strict"`, `"None"` or `"Lax"`. Defaults to `"Lax"` if not set. | 
+| `SESSION_COOKIE_MAX_AGE_SPAN` | Specifies, in second, how long the cookie should be valid for. Defaults to `604800` _(7 days)_ if not set. |
+| `SESSION_COOKIE_DOMAIN` | Can be used to specify a domain for the session cookie. |
+| `SESSION_COOKIE_PATH` | Can be used to specify a path for the session cookie. Defaults to `/` if not set. |
+
+[☝️ Back to summary](#summary)
+
+---
+
+## Generating a secret key
+
+Session cookies are signed using [HMAC SHA256](https://en.wikipedia.org/wiki/HMAC), which requires using a secret key of at least 32 bytes of length.
+This one-liner can be used to generate a random key, once the library is installed:
+
+```bash
+node -e "console.log(require('netlify-functions-session-cookie').generateSecretKey())"
+```
+
+Use the [`SESSION_COOKIE_SECRET` environment variable](#environment-variables-and-options) to give the library access to the secret key.
+
+[☝️ Back to summary](#summary)
+
+---
+
+## Notes and misc
+
+### Not affiliated with Netlify
+This open-source project is not affiliated with [Netlify](https://www.netlify.com/).
+
+### Usage with other AWS Lambda setups
+This library has been built for use with [Netlify Functions](https://docs.netlify.com/functions/build-with-javascript/), but could in theory work with other setups using AWS Lambda functions, depending on configuration specifics. 
+
+[☝️ Back to summary](#summary)
+
+---
+
+## Contributing
+
+> Work in progress 🚧. 
+>
+> **In the meantime:** Please feel free to open [issues](https://github.com/matteocargnelutti/netlify-functions-session-cookie/issues) to report bugs or suggest features.
+
+[☝️ Back to summary](#summary)
---
+++ index.js
@@ -0,0 +1,360 @@
+/** 
+ * @module netlify-functions-session-cookie
+ * @author Matteo Cargnelutti
+ * @license MIT
+ * @file index.js
+ * @description Cryptographically-signed session cookies for Netlify Functions.
+ */
+
+//------------------------------------------------------------------------------
+// Imports
+//------------------------------------------------------------------------------
+const crypto = require('crypto');
+const cookie = require('cookie');
+const Keygrip = require('keygrip');
+
+//------------------------------------------------------------------------------
+// Module-level constants
+//------------------------------------------------------------------------------
+/**
+ * Name to be used for the session cookie if none provided.
+ * @constant
+ * @private
+ */
+const SESSION_COOKIE_NAME_DEFAULT = 'session';
+
+/**
+ * Value to be used as a default for the "Max-Age" attribute of the session cookie.
+ * @constant
+ * @private
+ */
+const SESSION_COOKIE_MAX_AGE_SPAN_DEFAULT = (60 * 60 * 24 * 7);
+
+/**
+ * Length of the signature digest, in characters. 
+ * Used to separate signature from data in the raw session cookie.
+ * 
+ * @constant
+ * @private
+ */
+const SIGNATURE_DIGEST_LENGTH = 43;
+
+//------------------------------------------------------------------------------
+// Public functions 
+//------------------------------------------------------------------------------
+/**
+ * Binds a given lambda function handler to the `sessionWrapper()` function.
+ * 
+ * Usage:
+ * `exports.handler = withSession(async function(event, context, session) { ... }`
+ * 
+ * @param {function} handler - Lambda function handler. Must be async.
+ * @returns {function} - Copy of the `sessionWrapper` function bound to the `handler` function.
+ */
+function withSession(handler) {
+  if (handler.constructor.name !== "AsyncFunction") {
+    throw new Error(`"handler" must be an async function. ${handler.constructor.name} given.`);
+  }
+
+  return sessionWrapper.bind(handler);
+}
+
+/**
+ * Returns a reference to the `context.clientContext.sessionCookieData` object.
+ * This object contains data for the current session, which can be read and edited.
+ * 
+ * @param {Object} context - From the Lambda handler function.
+ * @returns {Object} - Reference to the session data object.
+ */
+function getSession(context) {
+  if (!context || !('clientContext' in context)) {
+    throw new Error('`getSession()` requires a valid Lambda `context` object as an argument.');
+  }
+
+  let session = context.clientContext.sessionCookieData;
+
+  // Initialize `sessionCookieData` if it doesn't exist.
+  if (session === undefined || session === null) {
+    context.clientContext.sessionCookieData = {};
+    session = context.clientContext.sessionCookieData;
+  }
+
+  return session;
+}
+
+/**
+ * Utility to help clear out a session object in place.
+ * 
+ * @param {Object} context - From the Lambda handler function.
+ * @public 
+ */
+function clearSession(context) {
+
+  const session = getSession(context);
+
+  for (const key in session) {
+    delete session[key];
+  }
+}
+
+/**
+ * Generates a 32-byte-long random key that can be used for signing cookies using SHA-256 HMAC.
+ * 
+ * Thanks to: https://github.com/crypto-utils/keygrip/issues/26
+ * 
+ * @returns {string} - Random series of 32 bytes encoded in base64.
+ * @public
+ */
+function generateSecretKey() {
+  return crypto.randomBytes(32).toString('base64');
+}
+
+//------------------------------------------------------------------------------
+// Local functions 
+//------------------------------------------------------------------------------
+/**
+ * Main wrapper around the lambda handler function.
+ * Automatically manages a cryptographically-signed session cookie, in an out. 
+ * Gives access to a `session` object, which can be used to access and edit session data.
+ * 
+ * Cookie data format:
+ * - Part 1: HMAC SHA256 digest of JSON string, base64.
+ * - Part 2: JSON string that was signed, base64.
+ * - See `SIGNATURE_DIGEST_LENGTH` to determine split between the two parts.
+ * 
+ * @param {Object} event - From the Lambda handler function.
+ * @param {Object} context - From the Lambda handler function.
+ * @this {function} - Lambda function handler. Bound via `withSession`.
+ * @returns {Object} - Altered `response` received from `handler`.
+ * @private
+ */
+async function sessionWrapper(event, context) {
+  const cookieName = getCookieName();
+  const secretKey = new Keygrip([getSecretKey()], 'sha256', 'base64');
+
+  let incomingCookies = null;
+  let response = null;
+
+  const session = getSession(context); // Holds the current state of session data.
+
+  //
+  // [1] Try to validate and parse current session data from the `Cookie` header.
+  // 
+
+  // Grab cookies from header
+  if (event.multiValueHeaders && event.multiValueHeaders.Cookie) {
+    incomingCookies = event.multiValueHeaders.Cookie;
+  }
+  if (event.multiValueHeaders && event.multiValueHeaders.cookie) {
+    incomingCookies = event.multiValueHeaders.cookie;
+  }
+
+  // Parse cookies, if any
+  if (incomingCookies) {
+    incomingCookies = cookie.parse(incomingCookies[0]);
+  }
+
+  // Grab, validate and parse session data from cookie
+  if (incomingCookies && incomingCookies[cookieName]) {
+
+    // Signature: first X characters (`SIGNATURE_DIGEST_LENGTH`), stays in base64.
+    // Note: Only works because we know that all characters used for signatures are 1 byte long.
+    let signature = incomingCookies[cookieName].substring(0, SIGNATURE_DIGEST_LENGTH);
+
+    // Data: everything after the signature. Needs to be decoded from base64, parsed from JSON.
+    let data = incomingCookies[cookieName].substring(SIGNATURE_DIGEST_LENGTH);
+    data = Buffer.from(data, 'base64').toString('utf-8');
+
+    // If signature matches, parse data from JSON and put into the `clientContext.session` object.
+    if (secretKey.verify(data, signature)) {
+      data = JSON.parse(data);
+      for (let [key, value] of Object.entries(data)) { // Update in place to preserve `session` ref.
+        session[key] = value
+      }
+    }
+
+  }
+
+  //
+  // [2] Execute the function handler. 
+  //
+  response = await this(event, context);
+  // `context` contains parsed session data that can be read and edited directly from the handler.
+
+  //
+  // [3] Process response out of the handler to automatically append session data as a signed cookie.
+  //
+
+  // Create `Set-Cookie` entry in `response.multiValueHeaders` if not set.
+  if (!response.multiValueHeaders) {
+    response.multiValueHeaders = {};
+  }
+
+  if (!response.multiValueHeaders['Set-Cookie']) {
+    response.multiValueHeaders['Set-Cookie'] = [];
+  }
+
+  // Merge any value that may be in `headers['Set-Cookie']` to `response.multiValueHeaders['Set-Cookie']`.
+  if (response.headers && response.headers['Set-Cookie']) { 
+    response.multiValueHeaders['Set-Cookie'].push(response.headers['Set-Cookie']);
+    delete response.headers['Set-Cookie'];
+  }
+
+  // Sign session data and add it to `Set-Cookie`.
+  let sessionAsJSON = JSON.stringify(session);
+  let cookieValue = secretKey.sign(sessionAsJSON) + Buffer.from(sessionAsJSON).toString('base64'); // session=[signature][data]
+
+  response.multiValueHeaders['Set-Cookie'].push(
+    cookie.serialize(cookieName, cookieValue, getCookieOptions())
+  );
+
+  return response;
+}
+
+/**
+ * Returns the name to be used for the session cookie. 
+ * 
+ * Defaults to the value of `SESSION_COOKIE_NAME_DEFAULT`.
+ * Can be overridden via `env.SESSION_COOKIE_NAME`.
+ * Will throw if `SESSION_COOKIE_NAME` is set but contains anything else than ASCII characters (excluding whitespace).
+ * 
+ * @returns {string} - Name used for the session cookie
+ * @private
+ */
+function getCookieName() {
+  let name = SESSION_COOKIE_NAME_DEFAULT;
+  const nameFromEnv = process.env.SESSION_COOKIE_NAME;
+
+  // Try to grab session cookie name from `SESSION_COOKIE_NAME` if available.
+  if (typeof nameFromEnv == 'string') {
+    const regex = /[A-Za-z0-9\!\#\$\%\&\'\*\+\-\.\^\_\`\|\~]+/g;
+    const check = nameFromEnv.match(regex);
+
+    if (nameFromEnv.length < 1) {
+      throw new Error(`"SESSION_COOKIE_NAME" cannot be an empty string.`);
+    }
+
+    if (!check instanceof Array || check[0] !== nameFromEnv) {
+      throw new Error(`"SESSION_COOKIE_NAME" must only contain ASCII characters and no whitespace.`);
+    }
+
+    name = nameFromEnv;
+  }
+
+  return name;
+}
+
+/**
+ * Builds an option object to be used by the cookie serializer.
+ * All options have defaults which can be edited using environment variables.
+ * 
+ * Environment variables available:
+ * - `env.SESSION_COOKIE_HTTPONLY`: 
+ *   Specifies if the cookie should have the `HttpOnly` attribute.
+ *   Set to "0" to remove this attribute from the cookie definition.
+ * - `env.SESSION_COOKIE_SECURE`:
+ *   Specifies if the cookie should have the `Secure` attribute.
+ *   Set to "0" to remove this attribute from the cookie definition.
+ * - `env.SESSION_COOKIE_SAMESITE`:
+ *   Will specify the value for the `SameSite` attribute for the cookie.
+ *   Can be "Strict", "None" or "Lax" (default).
+ * - `env.SESSION_COOKIE_MAX_AGE_SPAN`:
+ *   Specifies, in second, how long the cookie should be valid for. 
+ *   Defaults to 7 days.
+ * - `env.SESSION_COOKIE_DOMAIN`:
+ *   If set, will specify a value for the `Domain` attribute for the cookie.
+ * - `env.SESSION_COOKIE_PATH`:
+ *   If set, will specify a value for the `Path` attribute for the cookie.
+ *   Defaults to `/`.
+ * 
+ * @returns {object} - Options object for `cookie.serialize`
+ * @private
+ */
+function getCookieOptions() {
+  // Defaults (options detail: https://github.com/jshttp/cookie#options-1)
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'lax',
+    maxAge: SESSION_COOKIE_MAX_AGE_SPAN_DEFAULT,
+    path: '/'
+  }
+
+  //
+  // Use environment variables to edit defaults.
+  //
+  const {
+    SESSION_COOKIE_HTTPONLY,
+    SESSION_COOKIE_SECURE,
+    SESSION_COOKIE_SAMESITE,
+    SESSION_COOKIE_MAX_AGE_SPAN,
+    SESSION_COOKIE_DOMAIN,
+    SESSION_COOKIE_PATH
+  } = process.env;
+
+  // HttpOnly
+  if (SESSION_COOKIE_HTTPONLY === '0') {
+    delete options.httpOnly;
+  }
+
+  // Secure
+  if (SESSION_COOKIE_SECURE === '0') {
+    delete options.secure;
+  }
+
+  // SameSite
+  if (['Strict', 'Lax', 'None'].includes(SESSION_COOKIE_SAMESITE)) {
+    options.sameSite = SESSION_COOKIE_SAMESITE.toLowerCase();
+  }
+
+  // Max-Age
+  if (!isNaN(parseInt(SESSION_COOKIE_MAX_AGE_SPAN))) {
+    options.maxAge = parseInt(SESSION_COOKIE_MAX_AGE_SPAN);
+  }
+
+  // Domain
+  if (SESSION_COOKIE_DOMAIN) {
+    options.domain = SESSION_COOKIE_DOMAIN;
+  }
+
+  // Path
+  if (SESSION_COOKIE_PATH) {
+    options.path = SESSION_COOKIE_PATH;
+  }
+
+  return options;
+}
+
+/**
+ * Checks and returns the secret key to be used to sign the session cookie.
+ * Reads from `env.SESSION_COOKIE_SECRET`. 
+ * The key must be at least 32 bytes long.
+ * 
+ * @returns {string} - Key to be used to sign cookies
+ * @private
+ */
+function getSecretKey() {
+  let secret = process.env.SESSION_COOKIE_SECRET;
+  let secretLength = 0;
+
+  if (!secret || !secret instanceof String) {
+    throw new Error(`"SESSION_COOKIE_SECRET": No secret key provided.`);
+  }
+
+  secretLength = Buffer.byteLength(secret, 'utf-8');
+  if (secretLength < 32) {
+    throw new Error(`"SESSION_COOKIE_SECRET": The secret key must be at least 32 bytes long (${secretLength} given).`);
+  }
+
+  return secret;
+}
+
+//------------------------------------------------------------------------------
+// Exports
+//------------------------------------------------------------------------------
+module.exports = {
+  withSession,
+  getSession,
+  clearSession,
+  generateSecretKey
+}
